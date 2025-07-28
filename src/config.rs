3f5832c9c979382f1a0e0fd3db59b53bd8d3471d
lonedespair <lonelydespair1988@gmail.com>
2025-07-28 13:21:11 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.wvv.ip-ddns.com"];
+pub const RS_PUB_KEY: &str = "V3xh69FVc3nK3MSEfDNH3jOFUkA3GU4k3JOVXpztW1I=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
